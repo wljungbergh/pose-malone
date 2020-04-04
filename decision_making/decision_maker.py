c6d14e509211e719413020b835ef4849e7d14c43
Georg Hess <hessgeorg9@gmail.com>
2020-04-04 16:29:29 +0200

    Decision maker v0.2

--- decision_making/decision_maker.py
+++ decision_making/decision_maker.py
@@ -11,6 +11,7 @@ class Decision_maker:
         self.face_distance_time = np.zeros([N,1])
         self.face_distance_tol = distance_tolerance
 
+        # roll, pitch, yaw
         self.head_angles = np.zeros([N,3])
         self.head_angles_time = np.zeros([N,1])
         self.head_angles_tol = head_angle_tolerance
@@ -19,40 +20,64 @@ class Decision_maker:
         self.face_height_time = np.zeros([N,1])
         self.face_height_tol = face_height_tolerance
 
-        self.last_notifiation_time = time.time()
+        self.timestamps = np.zeros([N,1])
+
+        self.start_of_bad_posture = None
+        self.time_limit = time_limit
+
+        self.last_notification_time = time.time()
     
     def set_references(self, face_distance_ref, face_height_ref, head_angle_ref):
         self.face_distance_ref = face_distance_ref
         self.face_height_ref = face_height_ref
-        self.head_angle_ref = head_angle_ref
+        self.head_angles_ref = head_angle_ref
 
     def add_data(self, face_dist, head_angle, face_height):
-        timestamp = time.time()
-        self.add_face_distance(face_dist, timestamp)
-        self.add_head_angles(head_angle, timestamp)
-        self.add_face_height(face_height, timestamp)
+        self.add_face_distance(face_dist)
+        self.add_head_angles(head_angle)
+        self.add_face_height(face_height)
+        self.add_timestamp(time.time())
         return self.evalute_data()
 
-    def add_face_distance(self, new_face_distance, timestamp):
+    def add_face_distance(self, new_face_distance):
         self.face_distances = self.rotate_and_add_to_array(self.face_distances, new_face_distance)
-        self.face_distances_time = self.rotate_and_add_to_array(self.face_distances_time, timestamp)
 
-    def add_head_angles(self, new_head_angle, timestamp):
+    def add_head_angles(self, new_head_angle):
         self.head_angles = self.rotate_and_add_to_array(self.head_angles, new_head_angle)
-        self.head_angles_time = self.rotate_and_add_to_array(self.head_angles_time, timestamp)
     
-    def add_face_height(self, new_face_height, timestamp):
+    def add_face_height(self, new_face_height):
         self.face_height = self.rotate_and_add_to_array(self.face_height, new_face_height)
-        self.face_height_time = self.rotate_and_add_to_array(self.face_height_time, timestamp)
+
+    def add_timestamp(self, timestamp):
+        self.timestamps = self.rotate_and_add_to_array(self.timestamps, timestamp)
 
     def evalute_data(self):
         face_dist_ok = self.check_face_distance()
         face_height_ok = self.check_face_height()
         head_angle_ok = self.check_head_angle()
+
         if face_dist_ok and face_height_ok and head_angle_ok:
+            self.start_of_bad_posture = None
+            return True
+        else:
+            time_ok = self.check_time()
+        if time_ok:
             return True
         else:
             return False
+
+
+    def check_time(self):
+        if self.start_of_bad_posture is None and (self.timestamps[0] - self.last_notification_time) > self.time_limit:
+            self.start_of_bad_posture = self.timestamps[0]
+            return True
+        elif (self.timestamps[0] - self.start_of_bad_posture) < self.time_limit or (self.timestamps[0] - self.last_notification_time) < self.time_limit:
+            return True
+        else:
+            self.start_of_bad_posture = None
+            self.last_notification_time = time.time()
+            return False
+
 
     def check_face_distance(self):
         if np.abs(self.face_distance[0] - self.face_distance_ref) > self.face_distance_tol:
@@ -61,15 +86,18 @@ class Decision_maker:
             return True
 
     def check_face_height(self):
-        pass
+        if np.abs(self.face_height[0] - self.face_height_ref) > self.face_height_tol:
+            return False
+        else:
+            return True
 
     def check_head_angle(self):
-        pass
-
-    def find_face_distance_deviation(self):
-        face_distance_dev = np.max(self.face_distances) - np.min(self.face_distances)
-        if face_distance_dev > self.tol:
-            self.notify("Pose Malone", "Think about your posture :)")
+        relative_angles = self.head_angles[0]-self.head_angles_ref
+        off_angle = np.sqrt(relative_angles[0]**2 + relative_angles[1]**2)
+        if off_angle > self.head_angle_tol:
+            return False
+        else:
+            return True
 
     '''
     Sends a OS X style notification to the user
