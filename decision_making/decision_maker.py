132db362a6ad48699d49f7e7d03f3699f6b1cefa
Georg Hess <hessgeorg9@gmail.com>
2020-04-04 16:42:50 +0200

    Dec_maker v0.3

--- decision_making/decision_maker.py
+++ decision_making/decision_maker.py
@@ -8,16 +8,13 @@ class Decision_maker:
         N = samples_to_keep
 
         self.face_distance = np.zeros([N,1])
-        self.face_distance_time = np.zeros([N,1])
         self.face_distance_tol = distance_tolerance
 
         # roll, pitch, yaw
-        self.head_angles = np.zeros([N,3])
-        self.head_angles_time = np.zeros([N,1])
-        self.head_angles_tol = head_angle_tolerance
+        self.head_angle = np.zeros([N,3])
+        self.head_angle_tol = head_angle_tolerance
 
         self.face_height = np.zeros([N,1])
-        self.face_height_time = np.zeros([N,1])
         self.face_height_tol = face_height_tolerance
 
         self.timestamps = np.zeros([N,1])
@@ -27,23 +24,23 @@ class Decision_maker:
 
         self.last_notification_time = time.time()
     
-    def set_references(self, face_distance_ref, face_height_ref, head_angle_ref):
+    def set_references(self, face_distance_ref, head_angle_ref, face_height_ref):
         self.face_distance_ref = face_distance_ref
         self.face_height_ref = face_height_ref
-        self.head_angles_ref = head_angle_ref
+        self.head_angle_ref = head_angle_ref
 
     def add_data(self, face_dist, head_angle, face_height):
         self.add_face_distance(face_dist)
-        self.add_head_angles(head_angle)
+        self.add_head_angle(head_angle)
         self.add_face_height(face_height)
         self.add_timestamp(time.time())
         return self.evalute_data()
 
     def add_face_distance(self, new_face_distance):
-        self.face_distances = self.rotate_and_add_to_array(self.face_distances, new_face_distance)
+        self.face_distance = self.rotate_and_add_to_array(self.face_distance, new_face_distance)
 
-    def add_head_angles(self, new_head_angle):
-        self.head_angles = self.rotate_and_add_to_array(self.head_angles, new_head_angle)
+    def add_head_angle(self, new_head_angle):
+        self.head_angle = self.rotate_and_add_to_array(self.head_angle, new_head_angle)
     
     def add_face_height(self, new_face_height):
         self.face_height = self.rotate_and_add_to_array(self.face_height, new_face_height)
@@ -92,8 +89,8 @@ class Decision_maker:
             return True
 
     def check_head_angle(self):
-        relative_angles = self.head_angles[0]-self.head_angles_ref
-        off_angle = np.sqrt(relative_angles[0]**2 + relative_angles[1]**2)
+        relative_angle = self.head_angle[0]-self.head_angle_ref
+        off_angle = np.sqrt(relative_angle[0]**2 + relative_angle[1]**2)
         if off_angle > self.head_angle_tol:
             return False
         else:
@@ -111,3 +108,8 @@ class Decision_maker:
         array = np.roll(array, shift, axis)
         array[0] = new_element
         return array
+
+ds_maker = Decision_maker(10, 0.2, 19, 0.5, 30)
+ds_maker.set_references(2, np.array([0, 0, 0]), 0)
+no_notification_needed = ds_maker.add_data(0.2, np.array([0, 0, 0]), 0.1)
+print(1+1)
