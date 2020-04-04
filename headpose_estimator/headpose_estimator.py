4ee08ddb3e213a99aa74b993ef6bf2c6a651df20
William Ljungbergh <william.ljungbergh@hotmail.com>
2020-04-04 15:16:12 +0200

    implement HeadPoseEstimator class

--- headpose_estimator/headpose_estimator.py
+++ headpose_estimator/headpose_estimator.py
@@ -10,6 +10,81 @@ sys.path.append(os.path.abspath(os.path.join(os.getcwd(),'headpose_estimator/ext
 from lib.FSANET_model import *
 
 
+class HeadPoseEstimator:
+    def __init(self):
+        self.model = self.init_model()
+
+    def init_model(self):
+        self.imsize = 64
+        stage_num = [3,3,3]
+        lambda_local = 1
+        lambda_d = 1
+        img_idx = 0
+        detected = '' #make this not local variable
+        time_detection = 0
+        time_network = 0
+        time_plot = 0
+        skip_frame = 1 # every 5 frame do 1 detection and network forward propagation
+        ad = 0.6
+
+        #Parameters
+        num_capsule = 3
+        dim_capsule = 16
+        routings = 2
+        stage_num = [3,3,3]
+        lambda_d = 1
+        num_classes = 3
+        image_size = 64
+        num_primcaps = 7*3
+        m_dim = 5
+        S_set = [num_capsule, dim_capsule, routings, num_primcaps, m_dim]
+
+        model1 = FSA_net_Capsule(image_size, num_classes, stage_num, lambda_d, S_set)()
+        model2 = FSA_net_Var_Capsule(image_size, num_classes, stage_num, lambda_d, S_set)()
+        
+        num_primcaps = 8*8*3
+        S_set = [num_capsule, dim_capsule, routings, num_primcaps, m_dim]
+
+        model3 = FSA_net_noS_Capsule(image_size, num_classes, stage_num, lambda_d, S_set)()
+
+        print('Loading models ...')
+        model_file_location = 'headpose_estimator/external/FSA-Net'
+        weight_file1 = model_file_location + '/pre-trained/300W_LP_models/fsanet_capsule_3_16_2_21_5/fsanet_capsule_3_16_2_21_5.h5'
+        model1.load_weights(weight_file1)
+        print('Finished loading model 1.')
+        
+        weight_file2 = model_file_location + '/pre-trained/300W_LP_models/fsanet_var_capsule_3_16_2_21_5/fsanet_var_capsule_3_16_2_21_5.h5'
+        model2.load_weights(weight_file2)
+        print('Finished loading model 2.')
+
+        weight_file3 = model_file_location + '/pre-trained/300W_LP_models/fsanet_noS_capsule_3_16_2_192_5/fsanet_noS_capsule_3_16_2_192_5.h5'
+        model3.load_weights(weight_file3)
+        print('Finished loading model 3.')
+        print('Finished lading models ...')
+
+
+        inputs = Input(shape=(64,64,3))
+        x1 = model1(inputs) #1x1
+        x2 = model2(inputs) #var
+        x3 = model3(inputs) #w/o
+        avg_model = Average()([x1,x2,x3])
+        return Model(inputs=inputs, outputs=avg_model)
+
+    def estimate_headpose(self, face, render = False):
+        faces[0,:,:,:] = cv2.resize(face, (img_size, img_size))
+        faces[0,:,:,:] = cv2.normalize(faces[0,:,:,:], None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)        
+        
+        face = np.expand_dims(faces[0,:,:,:], axis=0)
+        p_result = model.predict(face)
+        face = face.squeeze()
+        img = None
+
+        if render:
+            img = draw_axis(input_img[yw1:yw2 + 1, xw1:xw2 + 1, :], p_result[0][0], p_result[0][1], p_result[0][2])
+            
+        return p_result, img
+
+
 
 
 def draw_axis(img, yaw, pitch, roll, tdx=None, tdy=None, size = 50):
